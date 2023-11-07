25c2dddac71bd43fb05ab8fb1744a41609fb603b
AhmedNafie <ahmedtareknafie@gmail.com>
2023-11-07 20:51:23 +0200

    Display news headlines

---
+++ NewsApp/NewsClient.swift
@@ -0,0 +1,37 @@
+//
+//  NewsClient.swift
+//  NewsApp
+//
+//  Created by Ahmed Nafie on 07/11/2023.
+//
+
+import Foundation
+
+class NewsClient {
+    
+    enum endPoints: String {
+        case news = "https://newsapi.org/v2/top-headlines?country=us&apiKey=ab7aad6758e64293ab42ab229e21ac9f"
+        
+        var url: URL {
+            return URL(string: self.rawValue)!
+        }
+    }
+    
+    class func requestNews(url: URL, completionHandler: @escaping (NewsResponse?, Error?) -> Void) {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completionHandler(nil, error)
+                }
+                return
+            }
+            let decoder = JSONDecoder()
+            let newsData = try! decoder.decode(NewsResponse.self, from: data)
+            
+            DispatchQueue.main.async {
+                completionHandler(newsData, nil)
+            }
+        }
+        task.resume()
+    }
+}
--- NewsApp/NewsTableViewController.swift
+++ NewsApp/NewsTableViewController.swift
@@ -9,15 +9,25 @@ import UIKit
 
 class NewsTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var newsTableView: UITableView!
+    private var news: NewsResponse?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NewsClient.requestNews(url: NewsClient.endPoints.news.url) { response, error in
+            self.news = response
+            self.newsTableView.reloadData()
+        }
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return news?.totalResults ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath)
-        cell.textLabel?.text = "Testing"
+        cell.textLabel?.text = news?.articles[indexPath.row].title
         return cell
     }
-    
 }
 
