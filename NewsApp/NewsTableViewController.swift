0718c69aa354e9c2044bbec4bae4276b44b9183b
AhmedNafie <ahmedtareknafie@gmail.com>
2023-11-07 17:35:44 +0200

    Added NewsTableView Functions

--- NewsApp/NewsTableViewController.swift
+++ NewsApp/NewsTableViewController.swift
@@ -7,13 +7,17 @@
 
 import UIKit
 
-class NewsTableViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class NewsTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath)
+        cell.textLabel?.text = "Testing"
+        return cell
+    }
+    
 }
 
