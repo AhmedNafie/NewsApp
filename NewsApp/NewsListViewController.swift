46f13db247b3a7ce70096e552ed71810e1b3b157
AhmedNafie <ahmedtareknafie@gmail.com>
2023-11-07 20:57:06 +0200

    Refactor NewsLIstViewController

--- NewsApp/NewsListViewController.swift
+++ NewsApp/NewsListViewController.swift
@@ -7,18 +7,18 @@
 
 import UIKit
 
-class NewsTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class NewsListViewController: UIViewController {
     
     @IBOutlet weak var newsTableView: UITableView!
     private var news: NewsResponse?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        NewsClient.requestNews(url: NewsClient.endPoints.news.url) { response, error in
-            self.news = response
-            self.newsTableView.reloadData()
-        }
+        fetchNews()
     }
+}
+
+extension NewsListViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return news?.totalResults ?? 0
@@ -31,3 +31,12 @@ class NewsTableViewController: UIViewController, UITableViewDelegate, UITableVie
     }
 }
 
+private extension NewsListViewController {
+    
+    func fetchNews() {
+        NewsClient.requestNews(url: NewsClient.endPoints.news.url) { response, error in
+            self.news = response
+            self.newsTableView.reloadData()
+        }
+    }
+}
