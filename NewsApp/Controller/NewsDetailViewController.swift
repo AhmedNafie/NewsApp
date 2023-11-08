2785b6388c19cc5056e58b093949cc95307c0d1e
AhmedNafie <ahmedtareknafie@gmail.com>
2023-11-08 09:59:14 +0200

    Added UI elements to NewsDetailViewController

--- NewsApp/Controller/NewsDetailViewController.swift
+++ NewsApp/Controller/NewsDetailViewController.swift
@@ -9,5 +9,11 @@ import UIKit
 
 class NewsDetailViewController: UIViewController {
     
-
+    @IBOutlet weak var headlinesLabel: UILabel!
+    @IBOutlet weak var newsImageView: UIImageView!
+    @IBOutlet weak var newsDetailTextView: UITextView!
+    
+    
+    @IBAction func submitButtonTapped(_ sender: Any) {
+    }
 }
