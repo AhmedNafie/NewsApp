5ae4f30e8a752f648d28ec8e96fafd43e18d1488
AhmedNafie <ahmedtareknafie@gmail.com>
2023-11-08 09:08:47 +0200

    Added NewsDetailViewController class

---
+++ NewsApp/Controller/NewsDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewsDetailViewController.swift
+//  NewsApp
+//
+//  Created by Ahmed Nafie on 08/11/2023.
+//
+
+import UIKit
+
+class NewsDetailViewController: UIViewController {
+    
+
+}
