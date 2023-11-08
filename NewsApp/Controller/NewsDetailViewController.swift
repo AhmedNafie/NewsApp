7e1ab29f51c4a621e147fc89dc224f149de9e32b
AhmedNafie <ahmedtareknafie@gmail.com>
2023-11-08 15:00:12 +0200

    Refactor NewsDetailViewController

--- NewsApp/Controller/NewsDetailViewController.swift
+++ NewsApp/Controller/NewsDetailViewController.swift
@@ -8,26 +8,28 @@
 import UIKit
 
 class NewsDetailViewController: UIViewController {
-    @IBOutlet weak var headlinesLabel: UILabel!
+    
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var newsImageView: UIImageView!
     @IBOutlet weak var contentLabel: UILabel!
-    
     @IBOutlet weak var ratingTextField: UITextField!
+    
     var article: Article?
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         configureArticle()
     }
     
     @IBAction func saveButtonTapped(_ sender: Any) {
-        let allowedRatings = 1...5
-        let rating = Int(ratingTextField.text ?? "0")
-        let message = allowedRatings.contains(rating ?? 0) ? Constants.Strings.ratingMessageSucessfull : Constants.Strings.ratingMessageFailed
-        presentAlert(title: Constants.Strings.ratingMessageTitle, message: message)
-        
+        validateRating()
     }
-    
+}
+
+private extension NewsDetailViewController {
     func configureArticle() {
+        titleLabel.text = article?.title
+        contentLabel.text = article?.description
         if let url = URL(string: article?.urlToImage ?? "") {
             NewsClient.requestImageFile(url: url) { image, error in
                 DispatchQueue.main.async {
@@ -35,9 +37,13 @@ class NewsDetailViewController: UIViewController {
                 }
             }
         }
-        
-        headlinesLabel.text = article?.title
-        contentLabel.text = article?.description
+    }
+    
+    func validateRating() {
+        let allowedRatings = 1...5
+        let rating = Int(ratingTextField.text ?? "0")
+        let message = allowedRatings.contains(rating ?? 0) ? Constants.Strings.ratingMessageSucessfull : Constants.Strings.ratingMessageFailed
+        presentAlert(title: Constants.Strings.ratingMessageTitle, message: message)
     }
     
     func presentAlert(title: String, message: String) {
